40cd7a9026ce52291c3e381c16060b115a12a5c0
juhi7782 <108575713+juhi7782@users.noreply.github.com>
2022-09-15 13:12:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hello");
+
+let outputscreen=document.getElementById("output-screen");
+
+function display(num){
+    outputscreen.value +=num;
+
+}
+
+function Calculator(){
+try{
+    outputscreen.value = eval(outputscreen.value);
+
+}
+catch(err)
+{
+
+   alert("invalid"); 
+}
+
+}
+
+function clear(){
+outputscreen.value = "";
+
+}
+
+function del(){
+    outputscreen.value=outputscreen.value.slice(0,-1);
+
+
+}
